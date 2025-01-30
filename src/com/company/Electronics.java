3e58898e85f90a75567a02ff0d8a49d6cddf87fe
Hubert.Karw <hubert_karwowski@o2.pl>
2025-01-30 21:43:29 +0100

    java/product-project/task2/product updates

--- src/com/company/Electronics.java
+++ src/com/company/Electronics.java
@@ -4,4 +4,15 @@ public class Electronics extends Product {
     public Electronics(int id, String name, double price, int stock) {
         super(id, name, price, stock);
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Electronics{");
+        sb.append("id=").append(this.getId());
+        sb.append(", name='").append(this.getName()).append('\'');
+        sb.append(", price=").append(this.getPrice());
+        sb.append(", stock=").append(this.getStock());
+        sb.append('}');
+        return sb.toString();
+    }
 }
