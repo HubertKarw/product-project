d29d018ae25081fa9b1fd68962241c0f3d8a3afa
Hubert.Karw <hubert_karwowski@o2.pl>
2025-02-02 16:47:34 +0100

    java/product-project/task5/Order functions Added setters, getter and to string to order class

--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -12,4 +12,38 @@ public class Order {
         this.client = cart.getClientName();
         this.totalPrice =cart.totalPrice();
     }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Order{");
+        sb.append("cart=").append(cart);
+        sb.append(", client='").append(client).append('\'');
+        sb.append(", totalPrice=").append(totalPrice);
+        sb.append('}');
+        return sb.toString();
+    }
 }
