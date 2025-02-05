e8dc18b3174129b272f93afc161eac7922bc8dfb
Hubert.Karw <hubert_karwowski@o2.pl>
2025-02-05 22:47:42 +0100

    java/product-project/task2/Update test

--- src/com/company/test/Test.java
+++ src/com/company/test/Test.java
@@ -42,7 +42,8 @@ public class Test {
         Product computer2 = new Computer(1,"Dell",123,100);
         Product computer3 = new Computer(1,"Dell",123,100);
         Product electronics = new Electronics(20,"cable", 10, 1);
-        Electronics[] accessories1 = {(Electronics) electronics};
+        List<Electronics> accessories1 = new ArrayList<>();
+        accessories1.add((Electronics) electronics);
         Product smartphone1 = new Smartphone(1,"samsung", 1000.1,10,"red", 1200);
         Product smartphone2 = new Smartphone(1,"samsung", 1000.1,10,"red", 1200, accessories1);
         List<Product> products = new ArrayList<>();
