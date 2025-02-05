001f10a1d683e208f74761108739ac387ca9d771
Hubert.Karw <hubert_karwowski@o2.pl>
2025-02-05 22:38:44 +0100

    java/product-project/task7/created ComandLine Interface created interface for comandLineImplementation

---
+++ src/com/company/CommandLine.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface CommandLine {
+    void printCart();
+    void printProducts();
+    void printOrder();
+    void addToCart();
+    void removeFromCart();
+
+
+}
