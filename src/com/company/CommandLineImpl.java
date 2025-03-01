250ec9beec23ce3decacb6cd9dcf1f5b5a359bce
Hubert.Karw <hubert_karwowski@o2.pl>
2025-03-01 09:22:28 +0100

    java/product-project/task7/formatting

--- src/com/company/CommandLineImpl.java
+++ src/com/company/CommandLineImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 public class CommandLineImpl implements CommandLine {
     Scanner scanner = new Scanner(System.in);
+
     @Override
     public void printCart(Cart cart) {
         System.out.println("Cart:");
@@ -24,18 +25,19 @@ public class CommandLineImpl implements CommandLine {
     }
 
     @Override
-    public Order createOrder(Cart cart){
-        System.out.println("Creating order form cart:\n "+ cart);
+    public Order createOrder(Cart cart) {
+        System.out.println("Creating order form cart:\n " + cart);
         return new Order(cart);
     }
+
     @Override
-    public void processOrder(Order order){
+    public void processOrder(Order order) {
         System.out.println("Processing Order");
         OrderProcessor.process(order);
     }
+
     @Override
     public void addToCart(ProductManager pm, Cart cart) {
-//        Scanner scanner = new Scanner(System.in);
         System.out.println("Give product ID you want to add to cart");
         String id = scanner.nextLine();
         System.out.println("Give product quantity you want to add to cart");
@@ -46,14 +48,12 @@ public class CommandLineImpl implements CommandLine {
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
-        pm.addProductToCart(cart, (UUID)UUID.fromString(id), quantity);
-        System.out.println("added product with id: "+quant+" to cart");
-        scanner.close();
+        pm.addProductToCart(cart, (UUID) UUID.fromString(id), quantity);
+        System.out.println("added product with id: " + quant + " to cart");
     }
 
     @Override
     public void removeFromCart(ProductManager pm, Cart cart) {
-//        Scanner scanner = new Scanner(System.in);
         System.out.println("Give product ID you want to remove from cart");
         String id = scanner.nextLine();
         System.out.println("Give product quantity you want to remove from cart");
@@ -65,7 +65,6 @@ public class CommandLineImpl implements CommandLine {
             e.printStackTrace();
         }
         pm.removeProductFromCart(cart, UUID.fromString(id), quantity);
-        System.out.println("removed product with id: "+id+" from cart");
-//        scanner.close();
+        System.out.println("removed product with id: " + id + " from cart");
     }
 }
