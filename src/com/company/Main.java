145ecbac3dd8f9ca0e3744587bd8cbe5b4d7e3ea
Hubert.Karw <hubert_karwowski@o2.pl>
2025-03-03 21:50:49 +0100

    java/product-project/task8/remove COMMENTS and unused imports

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,6 @@ package com.company;
 
 import com.company.test.Test;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
